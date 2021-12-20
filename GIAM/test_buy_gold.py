294c8f730ab07fb1d22c8501e0c6a9639c0fb45d
admin <1234>
2021-12-20 13:32:42 +0200

    test buy and request gold

---
+++ GIAM/test_buy_gold.py
@@ -0,0 +1,107 @@
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_prep(func):
+    def wrapper(driver, val, coin_list):
+
+        start_time = time.time()
+        driver.get('https://qa.giam.finance/cabinet/invoices')
+        wait = WebDriverWait(driver, 600)
+
+        try:
+            login = driver.find_element(By.ID, 'username')
+            login.clear()
+
+            pswd = driver.find_element(By.ID, "password")
+            pswd.clear()
+
+            login.send_keys('tyutyunnikalisha@gmail.com')
+            pswd.send_keys('Q123123qqq')
+
+            driver.find_element(By.CSS_SELECTOR, "button").click()
+            print(f"Тестируемое значение  ({val}):")
+            for coin in coin_list:
+                func(driver, coin, val)
+
+
+        except selenium.common.exceptions.NoSuchElementException:
+            print(f'Тестируемое значение:  ({val}):')
+            for coin in coin_list:
+                func(driver, coin, val)
+
+    return wrapper
+
+
+@test_prep
+def test_incorrect_gold_by(driver, coin, val):
+    driver.get('https://qa.giam.finance/cabinet/token-sale')
+    time.sleep(1)
+    driver.find_element(By.CLASS_NAME, coin).click()
+    value = driver.find_element(By.CSS_SELECTOR, 'input')
+    value.send_keys(val)
+    value.send_keys(Keys.DELETE)
+    time.sleep(1)
+
+    # time.sleep(5)
+    try:
+        driver.find_element(By.ID, 'buy_gold_btn').click()
+        # skip = driver.find_elements(By.CLASS_NAME,'warning-popup')
+        driver.find_element(By.CLASS_NAME, 'btns-wrap').click()
+        time.sleep(2)
+        driver.find_element(By.CSS_SELECTOR, 'button').click()
+        time.sleep(5)
+        res = driver.current_url
+        if res == 'https://qa.giam.finance/cabinet/invoices':
+            print(coin)
+            print('Fail(Request was sent)')
+            return False
+        elif (driver.find_element(By.CLASS_NAME, 'error_message')).text == 'Can not create a invoice':
+            print(coin)
+            print('OK (Can not create a invoice)')
+            return True
+
+    except selenium.common.exceptions.ElementNotInteractableException:
+        driver.find_element(By.ID, 'request_gold_btn').click()
+        driver.find_element(By.CSS_SELECTOR, 'button').click()
+        time.sleep(2)
+        res = driver.find_element(By.CLASS_NAME, 'success-popup')
+        if res.text == 'Request was sent.':
+            print(coin)
+            print('Fail(Request was sent)')
+            return False
+
+
+coin_list = ["RUB", "UAH", "TMT", "MNT"]
+# coin_list = ["EUR", 'XAF', "XOF", "RWF", "SLL", "GHS", "AOA", "RUB", "UAH", "TMT", "MNT"]
+
+driver = webdriver.Chrome()
+# # #1 Ввод числа в экспоненциальном виде +
+# val = 'e45'
+# test_incorrect_gold_by(driver, val, coin_list)
+#
+# # #2 Ввод большого числа +
+# val = '12030432414324000000'
+# test_incorrect_gold_by(driver, val, coin_list)
+#
+# # #3 Ввод числа меньше минимального от 0 до 10 +
+# val = '5'
+# test_incorrect_gold_by(driver, val, coin_list)
+#
+# # #4 Ввод отрицательно числа +
+# val = '-14'
+# test_incorrect_gold_by(driver, val, coin_list)
+#
+# # #5 Ввод нули +
+# val = '000000000'
+# test_incorrect_gold_by(driver, val, coin_list)
+#
+# #6 Ввод пустого поля
+# val = ''
+# test_incorrect_gold_by(driver, val, coin_list)
+driver.quit()
+
