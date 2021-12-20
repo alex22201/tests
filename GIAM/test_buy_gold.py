ee961537d6ec45e0e82a097d25b9fda641b3b0b5
admin <1234>
2021-12-20 14:05:29 +0200

    test buy and request gold

--- GIAM/test_buy_gold.py
+++ GIAM/test_buy_gold.py
@@ -7,8 +7,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_prep(func):
-    def wrapper(driver, val, coin_list):
-
+    def wrapper(driver, val, currency_list):
+        start_time = time.time()
         driver.get('https://qa.giam.finance/cabinet/invoices')
         wait = WebDriverWait(driver, 600)
         try:
@@ -23,23 +23,25 @@ def test_prep(func):
 
             driver.find_element(By.CSS_SELECTOR, "button").click()
             print(f"Тестируемое значение  ({val}):")
-            for coin in coin_list:
-                func(driver, coin, val)
+            for currency in currency_list:
+                func(driver, currency, val)
 
 
         except selenium.common.exceptions.NoSuchElementException:
             print(f'Тестируемое значение:  ({val}):')
-            for coin in coin_list:
-                func(driver, coin, val)
-
+            for coin in currency_list:
+                print(bool(driver, coin, val))
+        print("%s seconds " % (time.time() - start_time))
+        print("______________________________________")
     return wrapper
 
 
 @test_prep
-def test_incorrect_gold_by(driver, coin, val):
+def test_incorrect_gold_by(driver, currency, val):
+
     driver.get('https://qa.giam.finance/cabinet/token-sale')
     time.sleep(1)
-    driver.find_element(By.CLASS_NAME, coin).click()
+    driver.find_element(By.CLASS_NAME, currency).click()
     value = driver.find_element(By.CSS_SELECTOR, 'input')
     value.send_keys(val)
     value.send_keys(Keys.DELETE)
@@ -55,11 +57,11 @@ def test_incorrect_gold_by(driver, coin, val):
         time.sleep(5)
         res = driver.current_url
         if res == 'https://qa.giam.finance/cabinet/invoices':
-            print(coin)
-            print('Fail(Request was sent)')
+            print(currency)
+            print('Fail(Request was sent(Min))')
             return False
         elif (driver.find_element(By.CLASS_NAME, 'error_message')).text == 'Can not create a invoice':
-            print(coin)
+            print(currency)
             print('OK (Can not create a invoice)')
             return True
 
@@ -69,40 +71,46 @@ def test_incorrect_gold_by(driver, coin, val):
         time.sleep(2)
         res = driver.find_element(By.CLASS_NAME, 'success-popup')
         if res.text == 'Request was sent.':
-            print(coin)
-            print('Fail(Request was sent)')
+            print(currency)
+            print('Fail(Request was sent(Min))')
             return False
+        elif (driver.find_element(By.CLASS_NAME, 'error_message')).text == 'Can not create a invoice':
+            print(currency)
+            print('OK (Can not create a invoice)')
+            return True
 
 
-coin_list = ["RUB", "UAH", "TMT", "MNT"]
+currency_list = ["RUB", "UAH", "TMT", "MNT"]
 # coin_list = ["EUR", 'XAF', "XOF", "RWF", "SLL", "GHS", "AOA", "RUB", "UAH", "TMT", "MNT"]
 
 driver = webdriver.Chrome()
-# # #1 Ввод числа в экспоненциальном виде +
-# val = 'e45'
-# test_incorrect_gold_by(driver, val, coin_list)
-#
-# # #2 Ввод большого числа +
-# val = '12030432414324000000'
-# test_incorrect_gold_by(driver, val, coin_list)
-#
-# # #3 Ввод числа меньше минимального от 0 до 10 +
-# val = '5'
-# test_incorrect_gold_by(driver, val, coin_list)
-#
-# # #4 Ввод отрицательно числа +
-# val = '-14'
-# test_incorrect_gold_by(driver, val, coin_list)
-#
-# # #5 Ввод нули +
-# val = '000000000'
-# test_incorrect_gold_by(driver, val, coin_list)
-#
-# #6 Ввод пустого поля
-# val = ''
-# test_incorrect_gold_by(driver, val, coin_list)
-#
-# #7 Ввод точки '.'
+# #1 Ввод числа в экспоненциальном виде +
+val = 'e45'
+test_incorrect_gold_by(driver, val, currency_list)
+
+# #2 Ввод большого числа +
+val = '12030432414324000000'
+test_incorrect_gold_by(driver, val, currency_list)
+
+# #3 Ввод числа меньше минимального от 0 до 10 +
+val = '5'
+test_incorrect_gold_by(driver, val, currency_list)
+
+# #4 Ввод отрицательно числа +
+val = '-14'
+test_incorrect_gold_by(driver, val, currency_list)
+
+# #5 Ввод нули +
+val = '000000000'
+test_incorrect_gold_by(driver, val, currency_list)
+
+#6 Ввод пустого поля
+val = ''
+test_incorrect_gold_by(driver, val, currency_list)
+
+#7 Ввод точки '.'
 val = '.'
-test_incorrect_gold_by(driver, val, coin_list)
+test_incorrect_gold_by(driver, val, currency_list)
+print(('End tests'))
 driver.quit()
+
